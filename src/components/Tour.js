3a7805ec15b7413aaff043615e899f84e9ee685e
luka <luka.tuononen@metropolia.fi>
2024-01-29 15:49:08 +0200

    interation 4 done

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,5 +1,19 @@
-function Tour() {
-  return <div>Tour</div>;
-}
+const Tour = ({ id, image, info, price, name }) => {
+  return (
+    <article className='tour'>
+      <div className='tour-img-container'>
+        <img src={image} alt={name} />
+        <span className='close-btn'>
+          <i className={id}></i>
+        </span>
+      </div>
+      <div className='tour-info'>
+        <h3>{name}</h3>
+        <h4 className='tour-price'>{price}</h4>
+        <p>{info}</p>
+        <button className='btn'>not interested</button>
+      </div>
+    </article>
+  )};
 
 export default Tour;
